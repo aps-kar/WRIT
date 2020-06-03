985055153c3e1a99562e9d39c538a6a791bec029
aps-kar <party7dealer@yahoo.gr>
2020-06-03 17:31:04 +0300

    a

--- sw.js
+++ sw.js
@@ -2,6 +2,7 @@
 //https://github.com/davidbau/seedrandom
 //cdnjs.cloudflare.com/ajax/libs/seedrandom/2.4.3/seedrandom.min.js
 !function(a,b){function c(c,j,k){var n=[];j=1==j?{entropy:!0}:j||{};var s=g(f(j.entropy?[c,i(a)]:null==c?h():c,3),n),t=new d(n),u=function(){for(var a=t.g(m),b=p,c=0;a<q;)a=(a+c)*l,b*=l,c=t.g(1);for(;a>=r;)a/=2,b/=2,c>>>=1;return(a+c)/b};return u.int32=function(){return 0|t.g(4)},u.quick=function(){return t.g(4)/4294967296},u.double=u,g(i(t.S),a),(j.pass||k||function(a,c,d,f){return f&&(f.S&&e(f,t),a.state=function(){return e(t,{})}),d?(b[o]=a,c):a})(u,s,"global"in j?j.global:this==b,j.state)}function d(a){var b,c=a.length,d=this,e=0,f=d.i=d.j=0,g=d.S=[];for(c||(a=[c++]);e<l;)g[e]=e++;for(e=0;e<l;e++)g[e]=g[f=s&f+a[e%c]+(b=g[e])],g[f]=b;(d.g=function(a){for(var b,c=0,e=d.i,f=d.j,g=d.S;a--;)b=g[e=s&e+1],c=c*l+g[s&(g[e]=g[f=s&f+b])+(g[f]=b)];return d.i=e,d.j=f,c})(l)}function e(a,b){return b.i=a.i,b.j=a.j,b.S=a.S.slice(),b}function f(a,b){var c,d=[],e=typeof a;if(b&&"object"==e)for(c in a)try{d.push(f(a[c],b-1))}catch(a){}return d.length?d:"string"==e?a:a+"\0"}function g(a,b){for(var c,d=a+"",e=0;e<d.length;)b[s&e]=s&(c^=19*b[s&e])+d.charCodeAt(e++);return i(b)}function h(){try{var b;return j&&(b=j.randomBytes)?b=b(l):(b=new Uint8Array(l),(k.crypto||k.msCrypto).getRandomValues(b)),i(b)}catch(b){var c=k.navigator,d=c&&c.plugins;return[+new Date,k,d,k.screen,i(a)]}}function i(a){return String.fromCharCode.apply(0,a)}var j,k=this,l=256,m=6,n=52,o="random",p=b.pow(l,m),q=b.pow(2,n),r=2*q,s=l-1;if(b["seed"+o]=c,g(b.random(),a),"object"==typeof module&&module.exports){module.exports=c;try{j=require("crypto")}catch(a){}}else"function"==typeof define&&define.amd&&define(function(){return c})}([],Math);
+
 var ip_addr = "https://aps-kar.github.io/WRIT/";
 var url_key = -1, sw_trace = "", crypto_key, signature;
 var enc = new TextEncoder(), dec = new TextDecoder(); // utf-8
@@ -175,8 +176,13 @@ var lib_js = `(function lib_js()
 
         let resp = await _fetch("/trace", {body: JSON.stringify(ret), method: "POST"});
         let valid = parseInt(resp);  // 1 or 0
-        // if (!valid)
-          //console.log("page seed: " + rng_seed);
+        let tx = document.getElementById("tx");
+        if (!valid)
+            tx.value += "error\\n";
+        else
+            tx.value += "well done\\n";
+
+        //console.log("page seed: " + rng_seed);
 
         return valid;
     };
@@ -297,133 +303,125 @@ self.addEventListener("fetch", function(event)
 {
     let req = event.request;
     // console.log("SW: req url: " + req.url);
-    let url = req.url;
-    if(url.includes(ip_addr)){
-       if((url.match(/\//g) || []).length == 3){
-         url_post = url.split("/")
-         url_post = url_post[url_post.length-1]
-         var dest = ["lib.js", "ww.js", "id", "url_key", "rng_seed", "trace"];
-         //check if url include one of our functions
-         if(dest.includes(url_post)){
-            //perform only action in case when it contain our function postfix
-            console.log(url_post);
-            if (url_post == "lib.js"){
-              event.respondWith(
-                  new Response(lib_js, {"status": 200})
-              );
 
-            }else if (url_post == "ww.js"){
-              event.respondWith(
-                  new Response(create_ww(), {"status": 200})
-              );
+    if (req.url == ip_addr+"/lib.js")
+    {
+        event.respondWith(
+            new Response(lib_js, {"status": 200})
+        );
+    }
+    else if (req.url == ip_addr+"/ww.js")
+    {
+        event.respondWith(
+            new Response(create_ww(), {"status": 200})
+        );
+    }
+    else if (req.url == ip_addr+"/id")
+    {
+        event.respondWith(req.text().then((id) =>
+        {
+            if (id === ww.id)
+                return new Response("success!", {"status": 200});
+            else
+                return new Response("failure!", {"status": 400});
+        }));
+    }
+    else if (req.url == ip_addr+"/url_key")
+    {
+        if (url_key == -1)
+        {
+            req.text().then((key) =>
+            {
+                url_key = key;
+            });
+            event.respondWith(new Response("", {"status": 200}));
+        }
+        else
+            event.respondWith(new Response("", {"status": 404}));
+    }
+    else if (req.url == ip_addr+"/rng_seed")
+    {
+        event.respondWith(req.text()
+        .then((_func_count) => func_count = parseInt(_func_count))
+        .then(() =>
+        {
+            let seed_generator = new Math.seedrandom('added entropy!', {entropy: true});
+            let rng_seed = Math.abs(seed_generator.int32());
+            let prng = new Math.seedrandom(rng_seed);
+            debug_seed = rng_seed;
 
-            }else if (url_post == "id"){
-              event.respondWith(req.text().then((id) =>
-              {
-                  if (id === ww.id)
-                      return new Response("success!", {"status": 200});
-                  else
-                      return new Response("failure!", {"status": 400});
-              }));
-
-            }else if (url_post == "url_key"){
-              if (url_key == -1)
-              {
-                  req.text().then((key) =>
-                  {
-                      url_key = key;
-                  });
-                  event.respondWith(new Response("", {"status": 200}));
-              }
-              else
-                  event.respondWith(new Response("", {"status": 404}));
-
-            }else if (url_post == "rng_seed"){
-              event.respondWith(req.text()
-              .then((_func_count) => func_count = parseInt(_func_count))
-              .then(() =>
-              {
-                  let seed_generator = new Math.seedrandom('added entropy!', {entropy: true});
-                  let rng_seed = Math.abs(seed_generator.int32());
-                  let prng = new Math.seedrandom(rng_seed);
-                  debug_seed = rng_seed;
-
-                  sw_trace = "";
-                  for (let i = 0; i < func_count; i++)
-                  {
-                      sw_trace += Math.abs(prng.int32()) % func_count;
-                  }
-
-                  return new Response(rng_seed, {"status": 200});
-              }));
-
-            }else if (url_post == "trace"){
-              let stack_trace = layers = "";
-              let ret = request = {};
-              event.respondWith(req.text().then((r) => {ret = r})
-              .then(() =>
-              {
-                  ret = JSON.parse(ret);
-                  // console.log(ret);
-                  stack_trace = ret.trace; // there are .original and .trimmed versions
-                  layers = ret.numbers;
-                  request = JSON.stringify(ret.request);
-
-                  let buf_base = (enc.encode(request)).buffer;
-
-                  return new Promise(function(resolve, reject)
-                  {
-                      hmac_prep().then((key) => {crypto_key = key})
-                      .then(() => crypto.subtle.sign("HMAC", crypto_key, buf_base))
-                      .then((sig) => {signature = buf2hex(sig)})
-                      .then(() =>
-                      {
-                          let valid = 1;
-                          // console.log("swtrace="+sw_trace+", layers="+layers);
-                          // console.log(stack_trace.original);
-                          if (sw_trace !== layers)
-                          {
-                              //console.log("sw seed: " + debug_seed);
-                              valid = 0;
-                          }
-                          // console.log(request);
-                          let package = JSON.stringify({"data": request, "signature": signature, "valid": valid});
-                          // console.log("Package Size: " + new Blob([package]).size);
-                          // console.log(package);
-
-                          let args = {body: package, method: "POST", headers: {"Content-Type": "application/json"}};
-                          fetch("/sw_post", args);
-
-                          resolve(valid.toString());
-                      })
-                  });
-              })
-              .then((verdict) =>
-              {
-                  return new Response(verdict, {"status": 200});
-              }));
+            sw_trace = "";
+            for (let i = 0; i < func_count; i++)
+            {
+                sw_trace += Math.abs(prng.int32()) % func_count;
             }
 
-
-       }else{
-              //in case when we need to parse all requests to this domain from SW
-             // console.log(req.url + " => SW: responding from network");
-             console.log("Redirect the request SW!");
-             event.respondWith(fetch(req, {credentials: "same-origin"}).then((response) =>
-             {
-                 if (!response.ok)
-                 {
-                     // console.log("SW: auto-fetch response failed with status: " + response.status);
-                     return response;
-                 }
-                 else
-                 {
-                     // caches.open("v1").then((cache) => cache.put(req.url, new Response(response))); //optional caching
-                     return response;
-                 }
-             }));
-
-       }
+            return new Response(rng_seed, {"status": 200});
+        }));
     }
-  }
+    else if (req.url == ip_addr+"/trace")
+    {
+        let stack_trace = layers = "";
+        let ret = request = {};
+        event.respondWith(req.text().then((r) => {ret = r})
+        .then(() =>
+        {
+            ret = JSON.parse(ret);
+            // console.log(ret);
+            stack_trace = ret.trace; // there are .original and .trimmed versions
+            layers = ret.numbers;
+            request = JSON.stringify(ret.request);
+            // console.log(ret);
+
+            let buf_base = (enc.encode(request)).buffer;
+
+            return new Promise(function(resolve, reject)
+            {
+                hmac_prep().then((key) => {crypto_key = key})
+                .then(() => crypto.subtle.sign("HMAC", crypto_key, buf_base))
+                .then((sig) => {signature = buf2hex(sig)})
+                .then(() =>
+                {
+                    let valid = 1;
+                    // console.log("swtrace="+sw_trace+", layers="+layers);
+                    // console.log(stack_trace.original);
+                    if (sw_trace !== layers)
+                    {
+                        //console.log("sw seed: " + debug_seed);
+                        valid = 0;
+                    }
+                    // console.log(request);
+                    let package = JSON.stringify({"data": request, "signature": signature, "valid": valid});
+                    // console.log("Package Size: " + new Blob([package]).size);
+                    // console.log(package);
+
+                    let args = {body: package, method: "POST", headers: {"Content-Type": "application/json"}};
+                    fetch("/sw_post", args);
+
+                    resolve(valid.toString());
+                })
+            });
+        })
+        .then((verdict) =>
+        {
+            return new Response(verdict, {"status": 200});
+        }));
+    }
+    else
+    {
+        // console.log(req.url + " => SW: responding from network");
+        event.respondWith(fetch(req, {credentials: "same-origin"}).then((response) =>
+        {
+            if (!response.ok)
+            {
+                // console.log("SW: auto-fetch response failed with status: " + response.status);
+                return response;
+            }
+            else
+            {
+                // caches.open("v1").then((cache) => cache.put(req.url, new Response(response))); //optional caching
+                return response;
+            }
+        }));
+    }
 });
