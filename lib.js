6b3ff372dddef5c4854fa12b86ceeab94bb8452b
aps-kar <party7dealer@yahoo.gr>
2020-06-05 19:05:44 +0300

    asd

--- lib.js
+++ lib.js
@@ -159,14 +159,14 @@
         if (!func_count)
             func_count = 10;
 
-        let rng_seed = await _fetch("/rng_seed", {body: func_count.toString(), method: "POST"});
+        let rng_seed = await _fetch("/WRIT/rng_seed", {body: func_count.toString(), method: "POST"});
         rng_seed = parseInt(rng_seed);
       //console.log("pre seed: " + rng_seed);
 
         let ret = new_gen_trace(rng_seed, protected, func_count);
         console.log(ret);
 
-        let resp = await _fetch("/trace", {body: JSON.stringify(ret), method: "POST"});
+        let resp = await _fetch("/WRIT/trace", {body: JSON.stringify(ret), method: "POST"});
         let valid = parseInt(resp);  // 1 or 0
         let tx = document.getElementById("tx");
         if (!valid)
